a2309ac844fc5b088d44958a87ca03198a56bcd5
Altria <1850181035@qq.comn>
2022-09-03 15:42:06 +0800

    修改AssetTransferDisplayDto：新增Id和AssetId属性

--- Models/DisplayDto/AssetTransferDisplayDto.cs
+++ Models/DisplayDto/AssetTransferDisplayDto.cs
@@ -5,6 +5,8 @@ namespace DesktopS3API.Models.DisplayDto;
 
 public class AssetTransferDisplayDto
 {
+    public int Id { get; set; }
+    public int AssetId { get; set; }
     public int FromDepartmentId { get; set; }
     public int ToDepartmentId { get; set; }
     public DateTime TransferTime { get; set; }
